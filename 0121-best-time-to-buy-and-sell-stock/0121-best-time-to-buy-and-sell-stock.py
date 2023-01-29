2d07ad529a7def0ad0621989c0524e8620229f60
Praveen Kumar Sridhar <69740366+PraveenKumarSridhar@users.noreply.github.com>
2023-01-28 20:43:51 -0500

    Time: 1118 ms (62.95%), Space: 24.9 MB (96.63%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -3,8 +3,7 @@ class Solution:
         min_buy, max_profit = float('inf'), 0
         
         for curr_price in prices:
-            if curr_price < min_buy:
-                min_buy = curr_price
+            min_buy = min(curr_price, min_buy)
             
             max_profit = max(max_profit, curr_price - min_buy)
         return max_profit
